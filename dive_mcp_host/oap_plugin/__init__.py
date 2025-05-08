caa71f554b94c7269db07993e8837eb65adb3c32
Louis <louis@funmula.com>
2025-05-08 22:30:28 +0800

    OAP: http handler of OAPPlugin

--- dive_mcp_host/oap_plugin/__init__.py
+++ dive_mcp_host/oap_plugin/__init__.py
@@ -1,23 +1,25 @@
 import json
 from collections.abc import Callable
+from pathlib import Path
 from typing import Any
 
+from dive_mcp_host.env import DIVE_CONFIG_DIR
 from dive_mcp_host.httpd.conf.mcp_servers import (
-    Config,
     CurrentConfigHookName,
-    MCPServerConfig,
+    UpdateAllConfigsHookName,
 )
 from dive_mcp_host.oap_plugin.config_mcp_servers import MCPServerManagerPlugin
+from dive_mcp_host.oap_plugin.http_handlers import OAPHttpHandlers
 from dive_mcp_host.plugins.registry import PluginCallbackDef
 
 
 def get_static_callbacks() -> dict[str, tuple[Callable[..., Any], PluginCallbackDef]]:
     """Get the static callbacks."""
-
-    with open("oap_config.json", "r") as f:
+    with Path(DIVE_CONFIG_DIR, "oap_config.json").open("r") as f:
         oap_config = json.load(f)
 
     mcp_plugin = MCPServerManagerPlugin(oap_config["auth_key"])
+    handlers = OAPHttpHandlers(mcp_plugin)
 
     return {
         "get_mcp_configs": (
@@ -25,5 +27,15 @@ def get_static_callbacks() -> dict[str, tuple[Callable[..., Any], PluginCallback
             PluginCallbackDef(
                 hook_point=CurrentConfigHookName, callback="get_mcp_configs"
             ),
-        )
+        ),
+        "update_all_configs": (
+            mcp_plugin.update_all_config_callback,
+            PluginCallbackDef(
+                hook_point=UpdateAllConfigsHookName, callback="update_all_configs"
+            ),
+        ),
+        "http_routes": (
+            handlers.get_router,
+            PluginCallbackDef(hook_point="httpd.routers", callback="http_routes"),
+        ),
     }
--- dive_mcp_host/oap_plugin/config_mcp_servers.py
+++ dive_mcp_host/oap_plugin/config_mcp_servers.py
@@ -1,16 +1,22 @@
 """MCP Server configuration management in OAP Plugin."""
 
 import logging
-from contextlib import suppress
+import time
 from typing import Any, Literal
 
 import httpx
 
-from dive_mcp_host.httpd.conf.mcp_servers import Config, MCPServerConfig
+from dive_mcp_host.httpd.conf.mcp_servers import (
+    Config,
+    MCPServerConfig,
+    MCPServerManager,
+)
 from dive_mcp_host.oap_plugin.models import BaseResponse, UserMcpConfig
 
 OAP_ROOT_URL = "https://oap-hub.biggo.dev"
 logger = logging.getLogger("OAP_PLUGIN")
+
+MIN_REFRESH_INTERVAL = 60
 
 
 class MCPServerManagerPlugin:
@@ -19,21 +25,28 @@ class MCPServerManagerPlugin:
     def __init__(self, device_token: str = "") -> None:
         """Initialize the MCPServerConfigs from OAP."""
         self.device_token: str = device_token
-        self._configs: Config
+        self._user_mcp_configs: list[UserMcpConfig] = []
         self._refresh_ts: float = 0
         self._http_client = httpx.Client(
             base_url=OAP_ROOT_URL,
             headers={"Authorization": f"bearer {self.device_token}"},
         )
 
-    def update_device_token(self, device_token: str) -> None:
+    def update_device_token(
+        self, device_token: str, mcp_server_manager: MCPServerManager
+    ) -> None:
         """Update the device token and refresh the configs."""
         self.device_token = device_token
         self._http_client.headers = {"Authorization": f"bearer {self.device_token}"}
-        self.refresh()
+        self.refresh(mcp_server_manager)
 
-    def refresh(self) -> None:
+    def refresh(self, mcp_server_manager: MCPServerManager) -> None:
         """Refresh the MCP server configs."""
+        self._get_user_mcp_configs(refresh=True)
+        cfg = mcp_server_manager.current_config
+        # we already merged the configuration in callback function
+        assert cfg is not None
+        mcp_server_manager.update_all_configs(cfg)
 
     def update_all_config_callback(self, new_config: Config) -> Config:
         """Callback function for updating all configs."""
@@ -41,14 +54,13 @@ class MCPServerManagerPlugin:
 
     def current_config_callback(self, config: Config) -> Config:
         """Callback function for getting current config."""
-
         mcp_servers = self._get_user_mcp_configs()
         for server in mcp_servers:
             config.mcp_servers[server.name] = MCPServerConfig(
                 enabled=True,
                 url=server.url,
                 transport=server.transport,
-                headers=server.headers,
+                headers=server.headers,  # type: ignore
                 extraData={
                     "oap": {
                         "planTag": server.plan.lower(),
@@ -67,7 +79,15 @@ class MCPServerManagerPlugin:
         response = self._http_client.request(method, url)
         return BaseResponse[model].model_validate_json(response.text).data
 
-    def _get_user_mcp_configs(self) -> list[UserMcpConfig]:
+    def _get_user_mcp_configs(self, refresh: bool = False) -> list[UserMcpConfig]:
         """Get the user MCP configs."""
         url = "/api/v1/user/mcp/configs"
-        return self._send_api_request(url, "get", list[UserMcpConfig]) or []
+        if (
+            refresh
+            or not self._user_mcp_configs
+            or time.time() - self._refresh_ts > MIN_REFRESH_INTERVAL
+        ):
+            r = self._send_api_request(url, "get", list[UserMcpConfig]) or []
+            self._refresh_ts = time.time()
+            self._user_mcp_configs = r
+        return self._user_mcp_configs
