f1273213836ba060d58e1a30da3c8eb2f70f66f8
alex <alex@funmula.com>
2025-06-17 16:16:55 +0800

    fix: list tool cache with incorrect enable status

--- dive_mcp_host/httpd/routers/tools.py
+++ dive_mcp_host/httpd/routers/tools.py
@@ -5,6 +5,7 @@ from fastapi.responses import StreamingResponse
 from pydantic import ValidationError
 
 from dive_mcp_host.host.tools.model_types import ClientState
+from dive_mcp_host.httpd.conf.mcp_servers import Config
 from dive_mcp_host.httpd.dependencies import get_app
 from dive_mcp_host.httpd.routers.models import (
     McpTool,
@@ -53,10 +54,10 @@ async def list_tools(
     result: dict[str, McpTool] = {}
 
     # get full list of servers from config
-    if (config := await app.mcp_server_config_manager.get_current_config()) is not None:
-        all_servers = set(config.mcp_servers.keys())
+    if app.mcp_server_config_manager.current_config is not None:
+        all_server_configs = app.mcp_server_config_manager.current_config
     else:
-        all_servers = set()
+        all_server_configs = Config()
 
     # get tools from dive host
     for server_name, server_info in app.dive_host["default"].mcp_server_info.items():
@@ -77,7 +78,7 @@ async def list_tools(
         )
     logger.debug("active mcp servers: %s", result.keys())
     # find missing servers
-    missing_servers = all_servers - set(result.keys())
+    missing_servers = set(all_server_configs.mcp_servers.keys()) - set(result.keys())
     logger.debug("disabled mcp servers: %s", missing_servers)
 
     # get missing from local cache
@@ -94,6 +95,15 @@ async def list_tools(
         for server_name in missing_servers:
             if server_info := cached_tools.root.get(server_name, None):
                 server_info.enabled = False
+
+                # Sync tool 'enabled' with 'exclude_tools'
+                if mcp_config := all_server_configs.mcp_servers.get(server_name):
+                    for tool in server_info.tools:
+                        if tool.name in mcp_config.exclude_tools:
+                            tool.enabled = False
+                        else:
+                            tool.enabled = True
+
                 result[server_name] = server_info
             else:
                 logger.warning("Server %s not found in cached tools", server_name)
